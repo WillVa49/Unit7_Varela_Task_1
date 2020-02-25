8a0d53d4f78f8c384b771f351aaec670384c1f4b
WillVa49 <wilfrido.varela@my.tccd.edu>
2020-02-25 01:35:51 +0000

    Learning how to write structs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-//Unit7_Varela_Task_1 Dr_T 2-24-2020
+//Unit7_Varela_Task_1 Dr_T 2-24-2020 STRUCTS C++
 #include <iostream>
 using namespace std;
+//Declare structs above int main()
+
 
 int main() 
 {
-  std::cout << "Hello World!\n";
+
   return 0;
 }
